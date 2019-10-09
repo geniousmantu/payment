8172150743a45939f2225d6eac71dc6664b91eaf
Mantu Kumar <mkumar132@sapient.com>
2019-10-10 00:20:24 +0530

    Jenkins Tomcat fixes

--- src/main/java/com/sapient/payment/PaymentApplication.java
+++ src/main/java/com/sapient/payment/PaymentApplication.java
@@ -2,11 +2,19 @@ package com.sapient.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PaymentApplication {
+public class PaymentApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+		System.out.println("*********** PaymentApplication from Tomcat******");
 		SpringApplication.run(PaymentApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PaymentApplication.class);
+    }
 }
